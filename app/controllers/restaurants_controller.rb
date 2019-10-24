bc07762e4f012ca92b9657db59ddc57d055540dc
Paula Franichevich <pfranichevich94@gmail.com>
2019-10-24 11:23:15 -0300

    check

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,32 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :admin do
+    resources :restaurants, only: [:update, :edit, :destroy]
+  end
 end
